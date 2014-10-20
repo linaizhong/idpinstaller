b805de16556bfe21ba7779f7e5ce3f6c593e9113
Jason <j-tan@outlook.com>
2014-10-20 02:08:21 +0000

    Added root check and changed clone url

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
 yum -y install epel-release
 yum -y install git
 yum -y install ansible
 mkdir -p /opt/aaf-ansible
 cd /opt/aaf-ansible
-git clone git@github.com:j-tan/idp-playbook.git
+git clone https://github.com/j-tan/idp-playbook.git
 cd idp-playbook
 cp hosts /etc/ansible
 ansible-playbook software.yml
