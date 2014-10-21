2f997374c7c09e0180da5094ac71769c761a4dcb
Jason <j-tan@outlook.com>
2014-10-21 03:13:39 +0000

    Redirected stdout to log file, and added status messages
    Also moved filder creation task to the beginning.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,12 +4,28 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-yum -y install epel-release
-yum -y install git
-yum -y install ansible
+echo "### Creating directory /opt/aaf..."
 mkdir -p /opt/aaf
+echo "Complete"
+echo "- Navigating to /opt/aaf"
 cd /opt/aaf
-git clone https://github.com/ausaccessfed/idpinstaller.git
+echo "### Installing EPEL..."
+yum -y install epel-release &>>install.log
+echo "Complete"
+echo "### Installing git..."
+yum -y install git &>>install.log
+echo "Complete"
+echo "### Installing ansible..."
+yum -y install ansible &>>install.log
+echo "Complete"
+echo "### cloning from remote git repository..."
+git clone https://github.com/ausaccessfed/idpinstaller.git &>>install.log
+echo "- Navigating to repository"
 cd idpinstaller
+echo "### Copying over new ansible hosts file..."
 cp hosts /etc/ansible
-ansible-playbook software.yml
+echo "Complete"
+echo "### Running ansible playbook file..."
+ansible-playbook software.yml &>>../install.log
+echo "Complete"
+echo "For more detailed logs, view file install.log"
