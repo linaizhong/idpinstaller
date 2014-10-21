979e0c07672121402da7b80d12669820c1df6c6f
Jason Tan <j-tan@outlook.com>
2014-10-21 15:50:04 +1000

    Added output redirection to log files for status
    messages also. High level status messages should now turn up in log
    files.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,17 +5,18 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 wd=$(pwd)
-echo "### Initializing installation..."
+echo "### Initializing installation..." | tee -a $wd/install.log
 mkdir -p /opt/aaf
 cd /opt/aaf
-echo "### Installing prerequisite software..."
+echo "### Installing prerequisite software..." | tee -a $wd/install.log
 yum -y install epel-release &>>$wd/install.log
 yum -y install git &>>$wd/install.log
 yum -y install ansible &>>$wd/install.log
 git clone https://github.com/ausaccessfed/idpinstaller.git &>>$wd/install.log
 cd idpinstaller
 cp hosts /etc/ansible
-echo "### Installing your IdP... (This may take some time)"
+echo "### Installing your IdP... (This may take some time)" \
+     | tee -a $wd/install.log
 ansible-playbook software.yml &>>$wd/install.log
 echo "IdP installation complete."
-echo "For more detailed logs, view file install.log"
+echo "For more details, view install.log" | tee -a $wd/install.log
