a594a9aaccfa6db70ac3d2ab4b00d5880591333d
Jason Tan <j-tan@outlook.com>
2014-11-12 08:20:04 +1000

    Added case statements to handle LDAP related cases
    LDAP specific sections have been added to the case statements in
    anticipation of further work on LDAP connection functionality.

--- bootstrap.sh
+++ bootstrap.sh
@@ -41,6 +41,16 @@ user_input() {
           ENVIRONMENT_TYPE=$response
         fi
         ;;
+      "LDAP server name" )
+        LDAP_SERV_NAME=$response;;
+      "LDAP port" )
+        LDAP_PORT=$response;;
+      "LDAP distinguished name" )
+        LDAP_DN=$response;;
+      "LDAP password" )
+        LDAP_PASSWD=$response;;
+      "LDAP connection type" )
+        LDAP_CONN_TYPE=$response;;
     esac
   else
     # defaults should be selected
@@ -51,6 +61,16 @@ user_input() {
         IP_ADDR=$2;;
       "environment type" )
         ENVIRONMENT_TYPE=$2;;
+      "LDAP server name" )
+        LDAP_SERV_NAME=$2;;
+      "LDAP port" )
+        LDAP_PORT=$2;;
+      "LDAP distinguished name" )
+        LDAP_DN=$2;;
+      "LDAP password" )
+        LDAP_PASSWD=$2;;
+      "LDAP connection type" )
+        LDAP_CONN_TYPE=$2;;
     esac
   fi
 }
